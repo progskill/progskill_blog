7547fa6017a6d669f29074b8d5d1065ecc130fb1
Evans-mutuku <evansnyamai98@gmail.com>
2024-02-15 11:04:27 +0300

    adding variables

--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -1,29 +1,17 @@
 from django.shortcuts import render
 
 def home_screen(request):
-    context = {}
-    # Way One of using variables
-    # context['some_text'] = "This is some Important Text"
-    # context['some_number'] = 123456
+ print(request.headers)
+    # Passing Variables -  Method 1
 
-    # Method Two of Using Variables
 
-    # context = {
-    #     "some_text": "This is some Important Text",
-    #     "some_number": 123456,
-    # }
+   # Passing Variables -  Method 2
+
 
 
     # Passing a list of variables
 
-    list_variables = []
-    list_variables.append("Value 1")
-    list_variables.append("Value 2")
-    list_variables.append("Value 3")
-    list_variables.append("Value 4")
-    context['variable_list']  = list_variables
 
-
-    return render (request, 'personal/home.html', context)
+    return render (request, 'personal/home.html', {})
 
 # Create your views here.
